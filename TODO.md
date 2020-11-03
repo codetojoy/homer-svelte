6fb4a1676a3555648f26718bbd2721cef26320a8
Michael Easter <codetojoy@gmail.com>
2020-11-03 11:45:55 -0400

    cleaned up 'accordion' experiment and used a simple transition

--- TODO.md
+++ TODO.md
@@ -3,7 +3,9 @@ x _ read from txt file
 x _ remove jQuery
 x _ change to use stores
 x - add transient in local storage to a temp folder
+x - CSS animation to approximate accordion?
 
+- consider a way to close all sections (button on the side?) 
 - temp folder
     - drag n drop one link to the trash icon
 y - add admin section like Evan's scooter 
@@ -11,7 +13,6 @@ y - add admin section like Evan's scooter
 - admin section
     - drawer
     - or button that opens modal, like Evan's scooter 
-- CSS animation to approximate accordion?
 - some semblence of styling 
 - write tests
   - e.g. ./util/parse.js
--- src/Category.svelte
+++ src/Category.svelte
@@ -1,71 +1,27 @@
 <script>
-    import { linear } from 'svelte/easing';
-    import {blur, fade, fly, scale, slide} from 'svelte/transition';
+    import {linear}  from 'svelte/easing';
+    import {fade} from 'svelte/transition';
 
 	import Link from './Link.svelte';
 
 	export let category;
 
-	function mouseHandler() {
-	    this.classList.toggle("active");
-		const panel = this.nextElementSibling;
-		const newValue = (panel.style.display === "block") ? "none" : "block"; 
-		panel.style.display = newValue;
-		/*
-		if (panel.style.display === "block") {
-			panel.style.display = "none";
-		} else {
-			panel.style.display = "block";
-		}
-		*/
-	}
-
-	function addListeners(item, index) {
-		// item.addEventListener("mouseover", mouseHandler);
-		// the UI gets even more weird when we close on 'mouseleave'
-		// item.addEventListener("mouseleave", mouseHandler);
-	}
-
-	const accElements = document.getElementsByClassName("accordion");
-
-	[...accElements].forEach(addListeners);
-
-    const options = {duration: 1000, easing: linear};
+	const options = {duration: 800, easing: linear};
+	// TODO: make `showMode` an exported prop 
+	const showMode = true;
+	let show = false;
 </script>
 
 <section>
-	<h3 class="accordion">{category.name}</h3>
-	<div class="panel">
-		{#each category.links as link}
-            <div in:fly={{...options, opacity: 1, x: -400}}
-                 out:fly={{...options, opacity: 1, x: 500}}
-            >
-                <Link {link} />
-                <br/>
-            </div>
-		{/each}
-	</div>
+	<h3 on:mouseenter={() => show = true }>{category.name}</h3>
+	{#if (! showMode) || (showMode && show)}
+		<div class="panel">
+			{#each category.links as link}
+				<div transition:fade={options}>
+					<Link {link} />
+					<br/>
+				</div>
+			{/each}
+		</div>
+	{/if}
 </section>
-
-<style>
-/* 
-- this whole part is pretty dicey 
-- UX people will freak out
-- TODO: find a more Svelte-y way to do this 
-*/
-
-/*
-.accordion {
-  cursor: pointer;
-  transition: 0.4s;
-}
-
-.active {
-  background-color: wheat;
-}
-
-.panel {
-  display: none;
-}
-*/
-</style>
